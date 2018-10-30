0f669299895ad23986594a23e5ccee9cf6dcf0ee
Adriano <adriano@Mac-mini-de-Adriano.local>
2018-10-30 16:12:48 -0300

    cors added

--- src/config/server.js
+++ src/config/server.js
@@ -1,11 +1,13 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const cors = require('./cors')
 
 const PORT = 3003
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(cors)
 
 server.listen(PORT, function() {
     console.log(`Backend is running on port ${PORT}`)
