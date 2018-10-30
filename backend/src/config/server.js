affec75406770ef51a9aa8e372c4181f02e3d5fc
Adriano <adriano@Mac-mini-de-Adriano.local>
2018-10-30 15:40:22 -0300

    todo api

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -5,7 +5,10 @@ const PORT = 3003
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
 
 server.listen(PORT, function() {
     console.log(`Backend is running on port ${PORT}`)
 })
+
+module.exports = server
